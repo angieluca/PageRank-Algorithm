fe26023d34087be2688760c3687de3a27942a31a
Angela Luca <tennisangel22@outlook.com>
2024-11-06 14:22:02 -0500

    Testing adjancency matrix

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,43 @@
 #include <iostream>
-
+#include <map>
+#include <vector>
+#include <utility>
 #include "AdjacencyList.h"
 
-using namespace std;
-
 int main() {
-    int no_of_lines, power_iterations;
-    string from, to;
-    cin >> no_of_lines;
-    cin >> power_iterations;
-    for (int i = 0; i < no_of_lines; i++) {
-        cin >> from;
-        cin >> to;
-        // Do Something
+    int numLines, powerItr;
+    std::string fromPage, toPage;
+    std::cin >> numLines;
+    std::cin >> powerItr;
+    //adjacency matrix is key:string, value: vector of {string,int} pairs
+    std::map<std::string, std::vector<std::pair<std::string, int>>> adjMatrix;
+
+    //Get user input of graph and store in map for adjacency matrix
+    //Fill each element with zero
+    for (int i = 0; i < numLines; i++) {
+        std::cin >> fromPage;
+        std::cin >> toPage;
+        adjMatrix[fromPage].emplace_back(std::make_pair(toPage,0));
+        adjMatrix[toPage].emplace_back(std::make_pair(fromPage,0));
+        adjMatrix[fromPage].emplace_back(std::make_pair(fromPage,0));
+        adjMatrix[toPage].emplace_back(std::make_pair(toPage,0));
+    }
+
+    //Output adjacency matrix as a 2D array
+    //Output the Column Titles
+    for (auto element: adjMatrix){
+        std::cout << "\t" << element.first << " ";
+    }
+    std::cout << "\n";
+    //Output the actual data
+    for (auto element : adjMatrix){
+        std::cout << element.first << " ";
+        for (auto things: element.second){
+            std::cout << things.second << " ";
+        }
+        std::cout << "\n";
     }
     //Create a graph object
     // Created_Graph.PageRank(power_iterations);}
 }
+
