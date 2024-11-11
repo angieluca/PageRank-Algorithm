20ecab757816a4190e7c0419a23dd4a4893cfc14
Angela Luca <tennisangel22@outlook.com>
2024-11-11 18:31:16 -0500

    Finished code

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,101 @@
 #include <iostream>
 #include <map>
-#include <vector>
-#include <utility>
-#include "AdjacencyList.h"
+#include <set>
+#include <iomanip>
+
+void createAdjList(int &numLines, std::map<std::string, std::set<std::string>> &adjMap){
+    std::string fromPage, toPage;
+    //Get user input of website
+    for (int i = 0; i < numLines; i++) {
+        std::cin >> fromPage;
+        if (adjMap.find(fromPage) == adjMap.end()){
+            adjMap[fromPage] = std::set<std::string>(); //Insert "from" website with empty vector
+        }
+        std::cin >> toPage;
+        if (adjMap.find(toPage) == adjMap.end()) {
+            adjMap[toPage] = std::set<std::string>(); //Insert "to" website with empty vector
+        }
+        if (adjMap[fromPage].count(toPage) == 0){ //Check if website already in set
+            adjMap[fromPage].emplace(toPage);
+        }
+    }
+}
+
+void visualizeList(const std::map<std::string, std::set<std::string>> &adjMap){
+    //Visualization of adjacency list:
+    for (auto const &element: adjMap){
+        std::cout << element.first << " -> ";
+        for (auto const &thing: element.second){
+            std::cout << thing << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+std::map<std::string, float> createDefRanks(std::map<std::string,std::set<std::string>> &adjMap, float &pageRank){
+    //Make each website's rank default page rank
+    std::map<std::string, float> ranks;
+    for (const auto &website: adjMap){
+        ranks[website.first] = pageRank; //website.first means website name
+    }
+    return ranks;
+}
+
+void calculateRanks(std::map<std::string, std::set<std::string>> &adjMap, std::map<std::string, float> &ranks, std::map<std::string, float> &tempRanks, int &powerItr){
+    //Calculate ranks
+    float tempRank;
+    int iterations = 1;
+    while (iterations < powerItr){
+        //Go through each unique website, but we're actually calculating ranks of their out websites
+        for (const auto &website: adjMap){
+            int outdegree = website.second.size();
+            //Iterate through each indegree website and change their page rank
+            for (const auto &outWebs: website.second){
+                //Rank of "to" site will be ("from" site rank divided by "from" site's out degree)
+                float fromRank = ranks[website.first]; //rank of from site
+                tempRank = fromRank/outdegree;
+                tempRanks[outWebs] += tempRank;
+            }
+        }
+        for (const auto &website: adjMap){
+            ranks[website.first] = tempRanks[website.first];
+            tempRanks[website.first] = 0; //reset temp ranks
+        }
+        iterations++;
+    }
+}
+
+std::string printRanks(std::map<std::string, float> &ranks){
+    std::string output;
+    //Print out each website and rank
+    for (const auto &website: ranks){
+        std::cout << website.first << " " << std::fixed << std::setprecision(2) << website.second << std::endl;
+        output += website.first + " " + std::to_string(website.second) + "\n";
+    }
+    return output;
+}
 
 int main() {
+    std::map<std::string, std::set<std::string>> adjMap; //Adjacency List
+    std::map<std::string, float> tempRanks; //temporarily keep values during one iteration
+    std::map<std::string, float> ranks; //official ranks
+    float pageRank;
     int numLines, powerItr;
     std::string fromPage, toPage;
     std::cin >> numLines;
     std::cin >> powerItr;
-    //adjacency matrix is key:string, value: vector of {string,int} pairs
-    std::map<std::string, std::vector<std::pair<std::string, int>>> adjMatrix;
 
-    //Get user input of graph and store in map for adjacency matrix
-    //Fill each element with zero
-    for (int i = 0; i < numLines; i++) {
-        std::cin >> fromPage;
-        std::cin >> toPage;
-        adjMatrix[fromPage].emplace_back(std::make_pair(toPage,0));
-        adjMatrix[toPage].emplace_back(std::make_pair(fromPage,0));
-        adjMatrix[fromPage].emplace_back(std::make_pair(fromPage,0));
-        adjMatrix[toPage].emplace_back(std::make_pair(toPage,0));
-    }
+    createAdjList(numLines, adjMap);
+    //visualizeList(adjMap); //for debugging purposes
 
-    //Output adjacency matrix as a 2D array
-    //Output the Column Titles
-    for (auto element: adjMatrix){
-        std::cout << "\t" << element.first << " ";
-    }
-    std::cout << "\n";
-    //Output the actual data
-    for (auto element : adjMatrix){
-        std::cout << element.first << " ";
-        for (auto things: element.second){
-            std::cout << things.second << " ";
-        }
-        std::cout << "\n";
-    }
-    //Create a graph object
-    // Created_Graph.PageRank(power_iterations);}
+    //Default page rank of each is 1/(number of websites); remains when power iterator is 1
+    pageRank = (float)1/adjMap.size();
+    //Everything first has default ranks
+    ranks = createDefRanks(adjMap, pageRank);
+    //Calculate new ranks if powerItr > 1
+    if (powerItr > 1) calculateRanks(adjMap, ranks, tempRanks, powerItr);
+
+    std::string output = printRanks(ranks); //Print each website and their ranks
+
 }
 
--- test/test.cpp
+++ test/test.cpp
@@ -1,24 +1,25 @@
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
 
-// change if you choose to use a different header name
-#include "AdjacencyList.h"
-
 using namespace std;
 
 // the syntax for defining a test is below. It is important for the name to be
 // unique, but you can group multiple tests with [tags]. A test can have
 // [multiple][tags] using that syntax.
-TEST_CASE("Example Test Name - Change me!", "[tag]") {
-  // instantiate any class members that you need to test here
-  int one = 1;
 
-  // anything that evaluates to false in a REQUIRE block will result in a
-  // failing test
-  REQUIRE(one == 0); // fix me!
+TEST_CASE("Test 1 - Only one website", "[tag]") {
+    string input = R"(4 2
+google.com google.com
+google.com google.com
+google.com google.com
+google.com google.com)";
 
-  // all REQUIRE blocks must evaluate to true for the whole test to pass
-  REQUIRE(false); // also fix me!
+    string expectedOutput = "google.com 1.00";
+
+    string actualOutput;
+    REQUIRE (expectedOutput == actualOutput);
+
+
 }
 
 TEST_CASE("Test 2", "[tag]") {
